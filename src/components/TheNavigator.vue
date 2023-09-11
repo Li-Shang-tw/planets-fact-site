beed4f7fbb614ada3e27efa8d628e10672b19f1a
shang <vuvu0130@gmail.com>
2023-09-11 20:27:55 +0800

    navigator init

--- src/components/TheNavigator.vue
+++ src/components/TheNavigator.vue
@@ -1,9 +1,19 @@
 <script>
 
 </script>
+
 <template>
-    <nav>
-        
+    
+    <nav class="nav">
+        <h2 class="logo">THE PLANETS</h2>
+        <button class="nav__btn--toogle">
+            <img src="../assets/images/icon-hamburger.svg" alt="icon-hamburger">
+        </button>
+        <ul class="nav_list">
+            <li class="nav_item">
+                <router-link to="/">User</router-link>
+            </li>
+        </ul>
     </nav>
 </template>
 <style>
