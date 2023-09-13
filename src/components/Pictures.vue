6d7ea7f91bfc4292648e95b18bf619f677d5ce05
shang <vuvu0130@gmail.com>
2023-09-13 11:25:56 +0800

    picture component render image

--- src/components/Pictures.vue
+++ src/components/Pictures.vue
@@ -1,3 +1,27 @@
 <template>
-    
+    <img :src="imgUrl" alt="">    
 </template>
+<script>
+export default{
+    data(){
+        return{
+            imgUrl: this.formatUrl(this.imgUrlProp)
+        }
+    },
+    props:["imgUrlProp"],
+    watch:{
+        imgUrlProp(){            
+            this.imgUrl =  this.formatUrl(this.imgUrlProp);
+        }
+    },
+    methods:{
+        formatUrl(url){
+            let newUrl = "/src"+  url.slice(1);
+            return newUrl
+        }
+    }
+   
+   
+
+}
+</script>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,17 +1,17 @@
 <script >
 import sourceData from "../data.json"
 import SwitchBtn from "../components/SwitchBtn.vue"
-import Intro from "../components/Intro.vue"
-// import Pictures from "../components/Pictures.vue"
+import Intro from "../components/Intros.vue"
+import Pictures from "../components/Pictures.vue"
 import DataSection from "../components/DataSection.vue"
 export default{
-  components:{SwitchBtn,Intro,DataSection},
+  components:{SwitchBtn,Intro,DataSection,Pictures},
   data(){
     return{      
       planet:{},
       model:'overview',
       description:"",
-      url:"",
+      imgUrl:"",     
     }
   },
   props:['planetName'],
@@ -26,23 +26,26 @@ export default{
   methods:{
     updatePlanet(){
       this.planet = sourceData.find((item)=>item.name===this.planetName);
-      this.updateDescriptionAndUrl();    
+      this.updateData();    
     },
     changeModel(model){
       this.model =model;
-      this.updateDescriptionAndUrl();
+      this.updateData();
       
     },
-    updateDescriptionAndUrl(){
+    updateData(){
       if(this.model ==="overview"){
         this.description =this.planet.overview.content;
-        this.url =this.planet.overview.source;        
+        this.url =this.planet.overview.source; 
+        this.imgUrl = this.planet.images.planet;      
       }else if(this.model ==="structure"){
         this.description =this.planet.structure.content;
-        this.url =this.planet.structure.source;        
+        this.url =this.planet.structure.source;  
+        this.imgUrl = this.planet.images.internal;               
       }else if(this.model ==="geology"){
         this.description =this.planet.geology.content;
-        this.url =this.planet.geology.source;        
+        this.url =this.planet.geology.source; 
+        this.imgUrl = this.planet.images.geology;                    
       }
     }
 
@@ -52,7 +55,7 @@ export default{
 </script>
 
 <template >
-   
+    <Pictures class="pictures" :imgUrlProp="imgUrl" />
     <SwitchBtn class="btn" @passModel="changeModel"/>   
     <Intro class="intro" :planetProp ="planet" :descriptionProp="description" :urlProp="url"  :modelProp="model"/>    
     <DataSection class="data"/>
