c0597bee4fa936b0a3c001da7d083dfe0a7ba9ce
LiShang <vuvu0130@gmail.com>
2023-12-14 16:36:40 +0800

    fix:use css to replace js to change layout while changing of screen

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,58 @@
-<script >
-import { RouterLink, RouterView } from 'vue-router'
-import TheNavigator from "../src/components/TheNavigator.vue"
-export default{
-  components:{TheNavigator},
-  data(){
-    return{
-      showNavList:false
-    }    
-  }, 
-  computed:{
-    isDesktop(){
-      const clientWidth = document.body.clientWidth;
-      if(clientWidth <1200){
-        return false
-      }else{
-        return true
-      }     
-    }    
-  }, 
-  methods:{
-    ChangeshowNavList(){
-      this.showNavList =!this.showNavList;
-    }
-  }
-}
+<script>
+import { RouterView } from "vue-router";
+import TheNavigator from "../src/components/TheNavigator.vue";
+export default {
+  components: { TheNavigator },
+  data() {
+    return {
+      showNavList: false,
+    };
+  },
+  methods: {
+    ChangeshowNavList() {
+      this.showNavList = !this.showNavList;
+    },
+  },
+};
 </script>
 
 <template>
   <div class="wrap">
-   <div class="container">
-   <TheNavigator :showNavList="showNavList" @ChangeshowNavList="ChangeshowNavList"/>
-  
-   <div class="container2"> 
-    <!--只有手機板,會再顯示導覽列時,不渲染主畫面-->
-   <template v-if="isDesktop">
-    <RouterView />
-    </template>
-    <template v-else>
-      <RouterView  v-if="!showNavList"/>
-    </template>
-      
-    
-  </div> 
+    <div class="container">
+      <TheNavigator
+        :showNavList="showNavList"
+        @ChangeshowNavList="ChangeshowNavList"
+      />
 
-</div> 
-</div>  
-
+      <div class="container2">
+        <!--只有手機板,會再顯示導覽列時,不渲染主畫面-->
+        <div :class="{ main_hide: showNavList }"><RouterView /></div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style >
-.wrap{
-background-image: url("./assets/images/background-stars.svg");
+<style>
+.wrap {
+  background-image: url("./assets/images/background-stars.svg");
 }
-.container{
-  width:90%;
+.container {
+  width: 90%;
   margin: 0 auto;
 }
-
-@media(min-width:1200px){
-  .container2{
-  width:90%;
-  margin: 0 auto;
+.main_hide {
+  display: none;
 }
+@media (min-width: 600px) {
+  .main_hide {
+    display: block;
+  }
 }
 
+@media (min-width: 1200px) {
+  .container2 {
+    width: 90%;
+    margin: 0 auto;
+  }
+}
 </style>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,139 +1,159 @@
-<script >
-import sourceData from "../data.json"
-import SwitchBtn from "../components/SwitchBtn.vue"
-import Intro from "../components/Intros.vue"
-import Pictures from "../components/Picture.vue"
-import DataSection from "../components/DataSections.vue"
-export default{
-  components:{SwitchBtn,Intro,DataSection,Pictures},
-  data(){
-    return{      
-      planet:{},
-      model:'overview',
-      description:"",
-      imgOverview:"",
-      imgStructure:"",
-      imgGeology:"",
-      colorName:""    
-    }
+<script>
+import sourceData from "../data.json";
+import SwitchBtn from "../components/SwitchBtn.vue";
+import Intro from "../components/Intros.vue";
+import Pictures from "../components/Picture.vue";
+import DataSection from "../components/DataSections.vue";
+export default {
+  components: { SwitchBtn, Intro, DataSection, Pictures },
+  data() {
+    return {
+      planet: {},
+      model: "overview",
+      description: "",
+      imgOverview: "",
+      imgStructure: "",
+      imgGeology: "",
+      colorName: "",
+    };
   },
-  props:['planetName'],
-  computed:{
-    isDesktop(){
-      const clientWidth = document.body.clientWidth;
-      if(clientWidth <1200){
-        return false
-      }else{
-        return true
-      }     
-    }    
-  }, 
-  watch:{
-    planetName(){
-      this.updatePlanet();
-    }
-  },
-  mounted(){
-    this.updatePlanet();    
-  },
-  methods:{
-    updatePlanet(){
-      this.planet = sourceData.find((item)=>item.name===this.planetName);
-      //取得planet的三種model的圖片
-      this.imgOverview = this.planet.images.planet; 
-      this.imgStructure = this.planet.images.internal;    
-      this.imgGeology = this.planet.images.geology; 
-      this.updateData();
-      this.colorName = "--"+(this.planet.name.toLowerCase())    
-    },
-    changeModel(model){
-      this.model =model;
-      this.updateData();      
-    },
-    updateData(){
-      if(this.model ==="overview"){
-        this.description =this.planet.overview.content;
-        this.url =this.planet.overview.source;            
-      }else if(this.model ==="structure"){
-        this.description =this.planet.structure.content;
-        this.url =this.planet.structure.source;
-      }else if(this.model ==="geology"){
-        this.description =this.planet.geology.content;
-        this.url =this.planet.geology.source;            
-      }
-    }
-  }
-}
+  props: ["planetName"],
 
+  watch: {
+    planetName() {
+      this.updatePlanet();
+    },
+  },
+  mounted() {
+    this.updatePlanet();
+  },
+  methods: {
+    updatePlanet() {
+      this.planet = sourceData.find((item) => item.name === this.planetName);
+      //取得planet的三種model的圖片
+      this.imgOverview = this.planet.images.planet;
+      this.imgStructure = this.planet.images.internal;
+      this.imgGeology = this.planet.images.geology;
+      this.updateData();
+      this.colorName = "--" + this.planet.name.toLowerCase();
+    },
+    changeModel(model) {
+      this.model = model;
+      this.updateData();
+    },
+    updateData() {
+      if (this.model === "overview") {
+        this.description = this.planet.overview.content;
+        this.url = this.planet.overview.source;
+      } else if (this.model === "structure") {
+        this.description = this.planet.structure.content;
+        this.url = this.planet.structure.source;
+      } else if (this.model === "geology") {
+        this.description = this.planet.geology.content;
+        this.url = this.planet.geology.source;
+      }
+    },
+  },
+};
 </script>
 
-<template >
+<template>
   <div class="wrap">
-    
-    <Pictures class="pictures" :modelProp="model" :planetNameProp ="planet.name" />
-    <!--在不同的裝置使用不同的架構-->
-   <template v-if="isDesktop">
-    <div class="container3" >
-    <SwitchBtn class="btn" @passModel="changeModel" :modelProp="model" :colorNameProp="colorName" />   
-    <Intro class="intro" :planetProp ="planet" :descriptionProp="description" :urlProp="url"  :modelProp="model"/>    
-   </div>
-  </template>
-  <template v-else>   
-    <SwitchBtn class="btn" @passModel="changeModel" :modelProp="model" :colorNameProp="colorName" />   
-    <Intro class="intro" :planetProp ="planet" :descriptionProp="description" :urlProp="url"  :modelProp="model"/>    
-  </template> 
-  <DataSection class="data" :planetProp ="planet"/>
-  </div>  
+    <Pictures
+      class="pictures"
+      :modelProp="model"
+      :planetNameProp="planet.name"
+    />
+
+    <div class="container3">
+      <SwitchBtn
+        class="btn"
+        @passModel="changeModel"
+        :modelProp="model"
+        :colorNameProp="colorName"
+      />
+      <Intro
+        class="intro"
+        :planetProp="planet"
+        :descriptionProp="description"
+        :urlProp="url"
+        :modelProp="model"
+      />
+    </div>
+    <SwitchBtn
+      class="btn btn_hide"
+      @passModel="changeModel"
+      :modelProp="model"
+      :colorNameProp="colorName"
+    />
+    <Intro
+      class="intro btn_hide"
+      :planetProp="planet"
+      :descriptionProp="description"
+      :urlProp="url"
+      :modelProp="model"
+    />
+
+    <DataSection class="data" :planetProp="planet" />
+  </div>
 </template>
-<style >
-.wrap{
-  display:flex;
+
+<style>
+.wrap {
+  display: flex;
   flex-direction: column;
 }
-.btn{
-  order:-1;
+.btn {
+  order: -1;
+}
+.container3 {
+  display: none;
 }
 
-@media(min-width:600px){
-  .wrap{
-    flex-wrap:wrap;
+@media (min-width: 600px) {
+  .wrap {
+    flex-wrap: wrap;
     flex-direction: row;
     justify-content: space-between;
   }
-  .pictures{
-    width:100%;
+  .pictures {
+    width: 100%;
   }
-  .btn{
-   order:2;
-   width:45%
+
+  .btn {
+    order: 2;
+    width: 45%;
   }
-  .intro{
-    order:1;
-    width:45%
+  .intro {
+    order: 1;
+    width: 45%;
   }
-  .data{
-    order:3;
-    width:100%;
+  .data {
+    order: 3;
+    width: 100%;
   }
 }
 
-@media(min-width:1200px){  
-  .pictures{
-     width:45%;      
+@media (min-width: 1200px) {
+  .pictures {
+    width: 45%;
   }
-  .container3{
-    width:40%;
-    display:flex;
+  .container3 {
+    width: 40%;
+    display: flex;
     flex-direction: column;
   }
-  .btn{
-    width:100%;    
+  .btn {
+    width: 100%;
   }
-  .intro{
-      
-    width:100%;
+  .intro {
+    width: 100%;
   }
- 
+  .btn_hide {
+    display: none;
+  }
+  .intro_hide {
+    display: none;
+  }
 }
-
 </style>
